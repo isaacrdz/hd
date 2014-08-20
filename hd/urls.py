172fea5bd04ab1fb8b5cd444cbda579de27b0764
isaacrdz <zack_cpp@hotmail.com>
2014-08-20 10:47:31 -0500

    static

--- hd/urls.py
+++ hd/urls.py
@@ -25,6 +25,12 @@ urlpatterns = patterns('',
     url('', include('social.apps.django_app.urls', namespace='social')),
 
 
+
+    urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+
+
+
     url(r'^admin/', include(admin.site.urls)),
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
    
