1f208b16a625ffcbf42828342079e45916069e81
isaacrdz <zack_cpp@hotmail.com>
2014-08-21 16:18:14 -0500

    pp

--- home/views.py
+++ home/views.py
@@ -16,14 +16,3 @@ def log_out(request):
     logout(request)
     return redirect('enter')
 
-def get_user_avatar(strategy, details, response, uid, user, *args, **kwargs):
-    social = kwargs.get('social') or strategy.storage.user.get_social_auth(
-        strategy.backend.name,
-        uid
-    )
-    url = None
-    if strategy.backend.name == 'facebook':
-        url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
-
-    if url:
-        social.set_extra_data({'photo': url})
