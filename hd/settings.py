25eca4e86426f0784dccd4286a09fd2db2a56e21
isaacrdz <zack_cpp@hotmail.com>
2014-08-22 15:08:12 -0500

    j

--- hd/settings.py
+++ hd/settings.py
@@ -53,6 +53,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
     'social.apps.django_app.context_processors.backends',
     'social.apps.django_app.context_processors.login_redirect',
+    'django.contrib.auth.context_processors.auth',
 )
 
 MIDDLEWARE_CLASSES = (
